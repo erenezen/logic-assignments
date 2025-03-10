f00fa752ffdf655eef84c83ad87fc1b2eb9dc07a
erenezen <eren.ezen@yandex.com>
2025-03-10 10:16:05 +0300

    Update Full_Adder_tb.vhd

--- assignment3/Full_Adder_tb.vhd
+++ assignment3/Full_Adder_tb.vhd
@@ -5,11 +5,8 @@ entity full_adder_tb is
 end full_adder_tb;
 
 architecture test of full_adder_tb is
-    -- Signal declarations
     signal A, B, Cin : STD_LOGIC := '0';
     signal Sum, Cout : STD_LOGIC;
-    
-    -- Component declaration for the full adder
     component full_adder
         Port ( A    : in  STD_LOGIC;
                B    : in  STD_LOGIC;
@@ -19,45 +16,25 @@ architecture test of full_adder_tb is
     end component;
     
 begin
-    -- Instantiate the full adder
     UUT: full_adder port map (A => A, B => B, Cin => Cin, Sum => Sum, Cout => Cout);
-
-    -- Stimulus process
     stim_proc: process
     begin
-        -- Test case 1: A=0, B=0, Cin=0
         A <= '0'; B <= '0'; Cin <= '0';
         wait for 10 ns;
-        
-        -- Test case 2: A=0, B=0, Cin=1
         A <= '0'; B <= '0'; Cin <= '1';
         wait for 10 ns;
-        
-        -- Test case 3: A=0, B=1, Cin=0
         A <= '0'; B <= '1'; Cin <= '0';
         wait for 10 ns;
-        
-        -- Test case 4: A=0, B=1, Cin=1
         A <= '0'; B <= '1'; Cin <= '1';
         wait for 10 ns;
-        
-        -- Test case 5: A=1, B=0, Cin=0
         A <= '1'; B <= '0'; Cin <= '0';
         wait for 10 ns;
-        
-        -- Test case 6: A=1, B=0, Cin=1
         A <= '1'; B <= '0'; Cin <= '1';
         wait for 10 ns;
-        
-        -- Test case 7: A=1, B=1, Cin=0
         A <= '1'; B <= '1'; Cin <= '0';
         wait for 10 ns;
-        
-        -- Test case 8: A=1, B=1, Cin=1
         A <= '1'; B <= '1'; Cin <= '1';
         wait for 10 ns;
-
-        -- End of simulation
         wait;
     end process;
 end test;
